14eabcb5bfe2c07af891088a200e8464220a6ea8
Tigran Muradyan <muradyan@amediateka.ru>
2023-05-03 11:56:08 +0400

    Finish backend lint

--- board/api/apps/authentication/apis.py
+++ board/api/apps/authentication/apis.py
@@ -15,6 +15,11 @@ logger = getLogger(__name__)
 
 @router.get('csrftoken')
 def csrf_token(request: WSGIRequest) -> HttpResponse:
+    """Get CSRF Token
+
+    :param request: WSGIRequest
+    :return: HttpResponse
+    """
     response = HttpResponse()
     response.set_cookie('csrftoken', csrf.get_token(request))
     return response
