22f2cd3d0152d88d8cd1aceeb0520275d9b189d3
Konstantin Polsky <kpolsky@ntv.ru>
2023-06-08 18:37:46 +0300

    auth.js loginPath

--- board/frontend/lib/api/auth.js
+++ board/frontend/lib/api/auth.js
@@ -2,13 +2,15 @@ import Cookies from "js-cookie";
 
 import { setAuthorized } from "../store";
 
+const loginPath = `${global.BOARD_SUBPATH}/login`;
+
 export const isAuthorized = () => {
     return Cookies.get("authorized") ?? false;
 };
 
 export const authRequired = () => {
-    if (!isAuthorized() && window.location.pathname !== "/login") {
-        window.location.href = "/login";
+    if (!isAuthorized() && window.location.pathname !== loginPath) {
+        window.location.href = loginPath;
     }
 };
 
@@ -16,6 +18,6 @@ export const logout = () => {
     Cookies.remove("authorized");
     Cookies.remove("token");
 
-    window.location.href = "/login";
+    window.location.href = loginPath;
     setAuthorized(false);
 };
